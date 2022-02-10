e3ffee92d0c40555a162d50c4f971cb5ef1dba82
BaNgan <bangank36@gmail.com>
2022-02-10 09:14:22 +0000

    Create Story for testing

--- example/brazil-case-fires/config.js
+++ example/brazil-case-fires/config.js
@@ -1,6 +1,6 @@
 var config = {
-    style: 'mapbox://styles/branigan/cjzsvonse027m1co4nkxp13b3',
-    accessToken: 'pk.eyJ1IjoibWJ4c29sdXRpb25zIiwiYSI6ImNrMm01aG9hdTBlZGwzbXQ1ZXVrNHNmejAifQ.QHQA0N6XPWddCXtvoODHZg',
+    style: 'mapbox://styles/case-br/ckz8v1y06000t14o97ume3v5n',
+    accessToken: 'pk.eyJ1IjoiY2FzZS1iciIsImEiOiJja3RsaGcyN2IxdzQ0MnZxbmF6aDd2a2RvIn0.qS5KaXjOZA0Gz9rNgkuPuQ',
     showMarkers: false,
     theme: 'light',
     title: '',
@@ -10,32 +10,32 @@ var config = {
     chapters: [
         {
             id: 'phl',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Philadelphia Bicycle Infrastructure',
             image: '',
             description: 'Getting around Philadelphia on two wheels is fast, fun, and cheap. As a typical East Coast large city, the urban core is dense, so there is a lot within reach of a 15 minute ride... even mountain bike trails. Paired with the public transit infrastructure, cycling can be more efficient and much less expensive than driving (and parking) a car.',
             location: {
-                center: [-75.13080, 39.97790],
-                zoom: 9.83,
+                center: [-47.193, -4.725],
+                zoom: 6.42,
                 pitch: 0.00,
                 bearing: 0.00
             },
             onChapterEnter: [
                 {
-                    layer: 'tunnel-primary-secondary-tertiary-case',
-                    opacity: .45
+                    layer: 'Mining_Para copy',
+                    opacity: 1
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'phl-city-limits',
+                    layer: 'Conflict Areas_Maranhao_CPT',
                     opacity: 0
                 }
             ]
         },
         {
             id: 'bike-lanes',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Bike Lanes',
             image: '',
             description: 'Philadelphia has XX miles of bike lanes, XX miles of which are protected. Drivers are getting more used to sharing the road, but ride defensively.',
@@ -47,15 +47,15 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'phl-bike-network',
-                    opacity: 1
+                    layer: 'tunnel-major-link-case',
+                    opacity: 0
                 }
             ],
             onChapterExit: []
         },
         {
             id: 'indego',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Indego Bike Share',
             image: '',
             description: 'Indego has been operating in Philadelphia since 20XX. The system initally was focused on Center City, but has expanded service to neighboring areas to support equitable mobility options to the city\'s residents.',
@@ -67,20 +67,20 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'indego-stations',
+                    layer: 'National Forests_Para',
                     opacity: 0.8
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'indego-stations',
+                    layer: 'Extractive Reserves',
                     opacity: 0
                 }
             ]
         },
         {
             id: 'belmont',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Belmont Plateau Trails',
             image: '',
             description: 'A short ride along the Schuylkill River Trail from the Art Museum, Belmont is a twisty, log-ridden rollercoaster of a trail network. It is easy to get turned around, the underbrush is at times impenetrable, and short steep sections come out of nowhere. In other words, it\'s really fun',
@@ -92,20 +92,20 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'belmont',
+                    layer: 'Sustainable Development Projects',
                     opacity: 1
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'belmont',
+                    layer: 'Sustainable Development Projects',
                     opacity: 0
                 }
             ]
         },
         {
             id: 'wiss',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Wissahickon Park Trails',
             image: '',
             description: 'This steep, rocky gorge can be surprisingly technical. Follow the orange and yellow trails to repeatedly climb and descend through the schist hillsides (careful of the cliffs), or stick to the gravel Forbidden Drive for a relaxing ride along the creek. You\'ll forget you\'re in a city.',
@@ -117,20 +117,20 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'wissahickon',
+                    layer: 'turning-feature-outline',
                     opacity: 1
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'wissahickon',
+                    layer: 'road-minor-low',
                     opacity: 0
                 }
             ]
         },
         {
             id: 'pennypack',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Pennypack Park Trails',
             image: '',
             description: 'Pennypack is a great introduction trail system. Not too steep and not too technical, the beautiful wooded park also provides a great escape from urban life. The south side trails are originally bridle trails, so be nice to equestrians and dismount when you approach them.',
@@ -142,13 +142,13 @@ var config = {
             },
             onChapterEnter: [
                 {
-                    layer: 'pennypack',
-                    opacity: 1
+                    layer: 'road-motorway-trunk',
+                    opacity: 0
                 }
             ],
             onChapterExit: [
                 {
-                    layer: 'pennypack',
+                    layer: 'road-motorway-trunk',
                     opacity: 0
                 }
             ]
